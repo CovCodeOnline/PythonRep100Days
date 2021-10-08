9ec3e5bb7be3de8f746e3a87db3b2a349df5627f
CovCodeOnline <39882787+CovCodeOnline@users.noreply.github.com>
2021-10-08 18:22:20 +0100

    Day 1 End

--- Day 1/main.py
+++ Day 1/main.py
@@ -3,3 +3,15 @@ print(len(input("What is your name ?")))
 name = input("Phone Number")
 
 print(name)
+
+
+#1. Create a greeting for your program.
+print("Welcome do the band Name Generator")
+#2. Ask the user for the city that they grew up in.
+city = input("What city did you grew up? ")
+#3. Ask the user for the name of a pet.
+pet_name = input("What is yout pet name? ")
+#4. Combine the name of their city and pet and show them their band name.
+print("The Band name could be " + city + " " + pet_name)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
