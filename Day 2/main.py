5bf1d299d63322608b3f30516e3b0fade2cb540c
CovCodeOnline <39882787+CovCodeOnline@users.noreply.github.com>
2021-10-09 16:17:31 +0100

    Day 2 Lesson 1

---
+++ Day 2/main.py
@@ -0,0 +1,11 @@
+#Type ERROR, TYpe Chekung and Type Conversion
+
+#num_char = len(input("What is your name?"))
+#new_num_char = str (num_char)
+#print("Your name has " + new_num_char + " characters.")
+
+s = str(123)
+print(s[1])
+print (type(s))
+
+print(70 + float("100.5"))
